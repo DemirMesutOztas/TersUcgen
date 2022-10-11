993fd18693dd507c1e74641ce3caece1b221359f
Gogle Öztaş <demiroztas06@hotmail.com>
2022-10-11 11:34:37 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int n = input.nextInt();
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            for (int k = 1; k <= (n - i); k++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+
     }
 }
